858b4d4f3f2bab8053a8b859751345fb42f399fb
Eitraz <git@eitraz.com>
2015-12-29 17:35:32 +0100

    Evaluator updates

--- src/main/java/com/eitraz/automation/AutomationApplication.java
+++ src/main/java/com/eitraz/automation/AutomationApplication.java
@@ -18,9 +18,7 @@ import java.io.Serializable;
 
 public class AutomationApplication extends Application<AutomationConfiguration> implements RawDeviceEventListener {
     private HazelcastInstance hazelcast;
-    private IExecutorService evaluateExecutor;
     private TellstickAutomation tellstick;
-
     private static Evaluator evaluator;
 
     @Override
@@ -37,11 +35,7 @@ public class AutomationApplication extends Application<AutomationConfiguration> 
         this.tellstick = LifeCycleInstance.register(new TellstickAutomation(hazelcast));
 
         // Evaluator
-        evaluateExecutor = hazelcast.getExecutorService("evaluate-executor");
-        hazelcast.getConfig().getExecutorConfig("evaluate-executor").setPoolSize(1);
-        evaluator = new Evaluator(hazelcast, tellstick);
-
-        getTellstick().addRawDeviceEventListener(this);
+        evaluator = LifeCycleInstance.register(new Evaluator(hazelcast, tellstick));
 
         // Life cycle manager
         environment.lifecycle().manage(new Managed() {
@@ -51,14 +45,17 @@ public class AutomationApplication extends Application<AutomationConfiguration> 
 
                 // Setup tellstick - TODO: Replace this with Hazelcast configured devices
                 TellstickConfigurer.setup(tellstick.getTellstick(), automationConfiguration.getTellstick());
+
+                getTellstick().addRawDeviceEventListener(AutomationApplication.this);
             }
 
             @Override
             public void stop() throws Exception {
+                getTellstick().removeRawDeviceEventListener(AutomationApplication.this);
+
                 LifeCycleInstance.get().stop();
                 hazelcast.shutdown();
 
-                tellstick = null;
                 evaluator = null;
             }
         });
@@ -72,28 +69,8 @@ public class AutomationApplication extends Application<AutomationConfiguration> 
         return tellstick;
     }
 
-    private Member getMemberWithHighestPriority() {
-        int priority = Integer.MIN_VALUE;
-        Member member = null;
-        for (Member m : hazelcast.getCluster().getMembers()) {
-            Integer p = m.getIntAttribute("priority");
-            if (p > priority) {
-                priority = p;
-                member = m;
-            }
-        }
-        return member;
-    }
-
     @Override
     public void rawDeviceEvent(RawDeviceEvent event) {
-        evaluateExecutor.executeOnMember(new RequestEvaluation(), getMemberWithHighestPriority());
-    }
-
-    static class RequestEvaluation implements Runnable, Serializable {
-        @Override
-        public void run() {
-            evaluator.evaluate();
-        }
+        evaluator.requestEvaluation();
     }
 }
--- src/main/java/com/eitraz/automation/Evaluator.java
+++ src/main/java/com/eitraz/automation/Evaluator.java
@@ -1,38 +1,95 @@
 package com.eitraz.automation;
 
 import com.eitraz.automation.tellstick.TellstickAutomation;
+import com.eitraz.library.lifecycle.Startable;
+import com.eitraz.library.lifecycle.Stopable;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IExecutorService;
 import com.hazelcast.core.ILock;
+import com.hazelcast.core.Member;
 
-public class Evaluator {
+import java.io.Serializable;
+
+public class Evaluator implements Startable, Stopable {
     private HazelcastInstance hazelcast;
     private TellstickAutomation tellstick;
 
     private long lastEvaluation = 0;
     private ILock evaluateLock;
 
+    private IExecutorService evaluateExecutor;
+
+    private static Evaluator instance;
+
     public Evaluator(HazelcastInstance hazelcast, TellstickAutomation tellstick) {
+        // ... Well
+        if (instance != null)
+            throw new RuntimeException("A evaluator already exists!");
+        instance = this;
+
         this.hazelcast = hazelcast;
         this.tellstick = tellstick;
+
         evaluateLock = hazelcast.getLock("evaluate-lock");
+
+        evaluateExecutor = hazelcast.getExecutorService("evaluate-executor");
+        hazelcast.getConfig().getExecutorConfig("evaluate-executor").setPoolSize(1);
     }
 
     public TellstickAutomation getTellstick() {
         return tellstick;
     }
 
-    public void evaluate() {
+    public void requestEvaluation() {
+        evaluateExecutor.executeOnMember(new RequestEvaluation(), getMemberWithHighestPriority());
+    }
+
+    private void evaluate() {
         evaluateLock.lock();
         try {
             long time = System.currentTimeMillis();
             // Don't overdo it
             if (time - lastEvaluation > 1000) {
-                System.out.println("GOGOGOGO");
+                doEvaluation();
 
                 lastEvaluation = time;
             }
         } finally {
             evaluateLock.unlock();
+        }
+    }
+
+    private void doEvaluation() {
+
+    }
+
+    @Override
+    public void doStart() {
+
+    }
+
+    @Override
+    public void doStop() {
+
+    }
+
+    private Member getMemberWithHighestPriority() {
+        int priority = Integer.MIN_VALUE;
+        Member member = null;
+        for (Member m : hazelcast.getCluster().getMembers()) {
+            Integer p = m.getIntAttribute("priority");
+            if (p > priority) {
+                priority = p;
+                member = m;
+            }
+        }
+        return member;
+    }
+
+    static class RequestEvaluation implements Runnable, Serializable {
+        @Override
+        public void run() {
+            instance.evaluate();
         }
     }
 }
